02b196eee3b07cc50c5c376ad7b04c5593b93f17
adnanerlansyah403 <adnanerlansyah403@gmail.com>
2022-12-26 17:21:59 +0700

    first commit base project (+) install tailwindcss

---
+++ README.md
@@ -0,0 +1,9 @@
+# Q&A Errors
+
+## Installation
+- composer update
+- cp .env.example .env
+- sesuaikan database dengan driver
+- php artisan key:generate
+- npm install
+- npm run dev
---
+++ resources/views/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Q&A Errors</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body class="antialiased">
+
+        <h1 class="text-center">Hello World</h1>
+
+    </body>
+</html>
